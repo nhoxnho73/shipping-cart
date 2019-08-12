2384c474df29f8c549a331bdfeccd18acb525ade
cuongnd <37103436+cuongndatmarkcafe@users.noreply.github.com>
2019-08-12 18:08:04 +0700

    test training git

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,4 @@ OrderStatus.create! id: 1, name: "In Progress"
 OrderStatus.create! id: 2, name: "Placed"
 OrderStatus.create! id: 3, name: "Shipped"
 OrderStatus.create! id: 4, name: "Cancelled"
+
